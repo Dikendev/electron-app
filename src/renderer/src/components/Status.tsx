5cda6783d7d9c6b0c2f8b44854fb54e6585669bf
Dikendev <diken.dev@gmail.com>
2025-01-08 02:37:38 -0300

    Refactor status indentation

--- src/renderer/src/components/Status.tsx
+++ src/renderer/src/components/Status.tsx
@@ -2,36 +2,21 @@ interface StatusProps {
     credentialStatus: boolean
 }
 const Status = ({ credentialStatus }: StatusProps) => {
-
-
     return (
         <div className="status">
             {credentialStatus ? (
                 <>
-                    <div className="green">
-                    </div>
-
-                    <div className="status_item">
-                        credenciais
-                    </div>
+                    <div className="green"></div>
                 </>
             ) : (
                 <>
-                    <div className="red">
-                    </div>
-
-                    <div className="status_item">
-                        credenciais
-                    </div>
+                    <div className="red"></div>
                 </>
             )}
+            <div className="status_item"> credenciais</div>
 
-            <div className="green">
-            </div>
-
-            <div className="status_item">
-                internet
-            </div>
+            <div className="green"></div>
+            <div className="status_item">internet</div>
         </div>
     )
 }
