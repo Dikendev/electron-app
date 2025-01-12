2ed7ff7f328e1daee3ad2d67166d9da600ef062f
Dikendev <diken.dev@gmail.com>
2025-01-11 23:43:52 -0300

    Feature add key options interface

---
+++ src/types/automata/key-options.interface.ts
@@ -0,0 +1,9 @@
+type KeyOption =
+    "daysCol" |
+    "startTime1ndCol" |
+    "finishTime1ndCol" |
+    "startTime2ndCol" |
+    "finishTime2ndCol" |
+    "workingTimesCol"
+
+export type { KeyOption }
