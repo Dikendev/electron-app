82ec371d96655fbd43b2fd65d6f2746c4e8b86cc
Dikendev <diken.dev@gmail.com>
2025-01-12 16:47:48 -0300

    Feature add action component

--- src/renderer/src/components/Actions.tsx
+++ src/renderer/src/components/Actions.tsx
@@ -1,35 +1,93 @@
+import AppStatus from "../../../types/app-status.interface"
+import { AvailableCommands, SheetCellContentFilled } from "../../../types/automata"
+import { ExistOrNot } from "../../../types/automata/sheet-data.interface"
+
 interface ActionsProps {
-    credentialStatus: boolean
-    simulate: () => void
+    appStatus: AppStatus
+    sheetValues: SheetCellContentFilled
+    onClickAction: (options: AvailableCommands) => void
 }
 
-const Actions = ({ credentialStatus, simulate }: ActionsProps) => {
+const Actions = ({
+    appStatus,
+    onClickAction,
+    sheetValues
+}: ActionsProps): JSX.Element => {
     return (
         <div className="actions">
-            <div className="action">
-                <a>
-                    Início do expediente
-                </a>
-            </div>
+            <ActionStatus
+                appStatus={appStatus.credential}
+                sheetValue={sheetValues.startWorkingHours}
+                description="Início do expediente"
+                onClick={() => onClickAction('INICIO_EXP')}
+            />
 
-            <div className="action">
-                <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-                    Intervalo do almoço
-                </a>
-            </div>
+            <ActionStatus
+                appStatus={appStatus.credential}
+                sheetValue={sheetValues.startLunch}
+                description="Início do almoço"
+                onClick={() => onClickAction('INICIO_ALM')}
+            />
 
-            <div className={credentialStatus ? 'action' : 'action disabled'}>
-                <a target="_blank" rel="noreferrer" onClick={simulate}>
-                    Fim do almoço
-                </a>
-            </div>
+            <ActionStatus
+                appStatus={appStatus.credential}
+                sheetValue={sheetValues.finishLunch}
+                description="Fim do almoço"
+                onClick={() => onClickAction('FIM_ALM')}
+            />
 
-            <div className="action">
-                <a target="_blank" rel="noreferrer" onClick={simulate}>
-                    Fim do expediente
-                </a>
-            </div>
+            <ActionStatus
+                appStatus={appStatus.credential}
+                sheetValue={sheetValues.finishWorkingHours}
+                description="Fim do expediente"
+                onClick={() => onClickAction('FIM_EXP')}
+            />
         </div>
+    )
+}
+
+interface ActionStatusProps {
+    appStatus: boolean
+    sheetValue: ExistOrNot
+    description: string
+    onClick: (action: AvailableCommands) => void
+}
+
+const ActionStatus = ({
+    appStatus,
+    sheetValue,
+    description,
+    onClick
+}: ActionStatusProps): JSX.Element => {
+    return (
+        <div
+            style={{ position: 'relative' }}
+            className={appStatus ? 'action' : 'action disabled'}
+            onClick={() => onClick}>
+            <a>{description}</a>
+            <SheetValues sheetValues={sheetValue} />
+        </div>
+    )
+}
+
+interface SheetValuesProp {
+    sheetValues: ExistOrNot
+}
+
+const SheetValues = ({ sheetValues }: SheetValuesProp): JSX.Element => {
+    return (
+        <code
+            style={{
+                position: 'absolute',
+                top: -15,
+                left: 50,
+                right: 50,
+                width: '5rem',
+                textAlign: "center",
+                borderRadius: "0.3rem"
+            }}>
+            {sheetValues ? <p className="tip">{sheetValues}</p> : '-'}
+        </code>
     )
 }
 
