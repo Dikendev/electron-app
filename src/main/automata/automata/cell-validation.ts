69c62a547a949f5d4a204ed8fea7e893ebec3b23
Dikendev <diken.dev@gmail.com>
2025-01-12 17:36:40 -0300

    Feature add cell validation

---
+++ src/main/automata/automata/cell-validation.ts
@@ -0,0 +1,25 @@
+import { GoogleSpreadsheetCell } from "google-spreadsheet"
+import DateUtils from "../utils/date-utils"
+import { SheetData } from "../../../types/automata"
+
+class CellManager {
+    static isCellValid = (foundCell: Partial<SheetData>): boolean => {
+        const keys = Object.keys(foundCell)
+
+        for (const cell of keys) {
+            const keyData = cell as keyof typeof foundCell
+            const cellValue = foundCell[keyData]
+            if (cellValue === -1) return false
+        }
+        return true
+    }
+
+    static isCellDateEqualsTodayDate = (
+        cell: GoogleSpreadsheetCell,
+    ): boolean => {
+        const today = DateUtils.formatDateWithDayAndMonth()
+        return cell && cell.formattedValue === today
+    }
+}
+
+export default CellManager
