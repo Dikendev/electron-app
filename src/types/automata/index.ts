ce8f7bd677a26ad5e469ca6b35d74f692cacda8e
Dikendev <diken.dev@gmail.com>
2025-01-12 17:42:47 -0300

    Feature add barrel index file

---
+++ src/types/automata/index.ts
@@ -0,0 +1,13 @@
+import { AvailableCommands } from "./available-commands.interface"
+import { GoogleSheetCredentials, GoogleSheetAuthCredentials } from "./google.sheet.interface"
+import { KeyOption } from "./key-options.interface"
+import { SheetData, SheetCellContentFilled } from "./sheet-data.interface"
+
+export type {
+    AvailableCommands,
+    GoogleSheetCredentials,
+    GoogleSheetAuthCredentials,
+    KeyOption,
+    SheetData,
+    SheetCellContentFilled
+}
