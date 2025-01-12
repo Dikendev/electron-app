a2e8fe895a55e8513b3745b614bafa28535b39a0
Dikendev <diken.dev@gmail.com>
2025-01-11 23:43:38 -0300

    Feature add sheet data enum

---
+++ src/types/automata/sheet-data.enum.ts
@@ -0,0 +1,8 @@
+enum SheetDataEnum {
+    'DAYS' = 'DIAS',
+    'WORKING_HOUR_START' = 'HORÁRIO INÍCIO',
+    'WORKING_HOUR_FINISH' = 'HORÁRIO DE TÉRMINO',
+    'WORKING_HOURS' = 'HORAS TRABALHADAS'
+}
+
+export default SheetDataEnum
