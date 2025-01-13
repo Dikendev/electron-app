3aeca5704a982ff2100f7028dc504e59448ed9a0
Dikendev <diken.dev@gmail.com>
2025-01-13 01:07:07 -0300

    Feature add available options constants

---
+++ src/main/automata/constants/available-options.constant.ts
@@ -0,0 +1,8 @@
+const AVAILABLE_OPTIONS = {
+    INICIO_EXP: 'Início do expediente',
+    INICIO_ALM: 'Início do almoço',
+    FIM_ALM: 'Fim do almoço',
+    FIM_EXP: 'Fim do expediente'
+}
+
+export { AVAILABLE_OPTIONS }
