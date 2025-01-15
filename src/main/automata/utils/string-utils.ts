c18b5c8dfe7a15a1a2cd5d2654dacca4c7620dcb
Dikendev <diken.dev@gmail.com>
2025-01-14 23:48:31 -0300

    Feature add string utils to normalize and remove accents

---
+++ src/main/automata/utils/string-utils.ts
@@ -0,0 +1,12 @@
+class StringUtils {
+    static normalizeWord = (word: string): string => {
+        const removeLn = word.replace(/(\r\n|\n|\r)/gm, "");
+        return this.removeAccents(removeLn)
+    }
+
+    static removeAccents = (word: string): string => {
+        return word.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    }
+}
+
+export default StringUtils
