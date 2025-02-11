c55a3cf2ef09a5a63fad89b3638bea635e01e645
Dikendev <diken.dev@gmail.com>
2025-02-11 00:23:06 -0300

    Feature add Logs component for displaying log messages

---
+++ src/renderer/src/components/Logs.tsx
@@ -0,0 +1,23 @@
+interface Log {
+    level: string
+    message: string
+}
+
+type Logs = Log[]
+
+interface LogsProps {
+    logs: Logs
+}
+
+const Logs = ({ message, level }: Log): JSX.Element => {
+    return (
+        <>
+            <div>
+                message: {message}
+                level: {level}
+            </div>
+        </>
+    )
+}
+
+export default Logs
