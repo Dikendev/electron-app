be1fc8292701ad5a6d05aa9b6771ed6774d74afe
Dikendev <diken.dev@gmail.com>
2025-05-25 22:55:47 -0300

    fix: update Logo component to use correct credential property for image source

--- src/renderer/src/components/Logo.tsx
+++ src/renderer/src/components/Logo.tsx
@@ -4,13 +4,12 @@ import ia from '../assets/ia.svg'
 import iOff from '../assets/ia-off.png'
 
 interface LogoProps {
-    credential: AppStatus['credential']
+    credential: AppStatus
 }
 
 const Logo = ({ credential }: LogoProps) => {
-
     return (
-        <img alt="logo" className="logo" src={credential ? ia : iOff} />
+        <img alt="logo" className="logo" src={credential.today ? ia : iOff} />
     )
 }
 
